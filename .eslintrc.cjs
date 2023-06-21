1b4e6f7d3af1a70299be529b5e6cc1ec595f5979
Murilo Salemme Marega <murilo.marega@fretebras.com.br>
2023-06-21 12:50:00 -0300

    feat: add lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': 'error',
   },
-}
+};
