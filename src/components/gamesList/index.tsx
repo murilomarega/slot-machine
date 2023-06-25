a1595d6bb65ff76154c38643dea4365b575762bd
Murilo Salemme Marega <murilo.marega@fretebras.com.br>
2023-06-25 13:42:13 -0300

    feat: filter games

--- src/components/gamesList/index.tsx
+++ src/components/gamesList/index.tsx
@@ -1,12 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/useStore';
-import { fetchGamesList, getGames } from '../../store/slices/games';
+import { IGame } from '../../store/interfaces';
+import {
+  fetchGamesList,
+  getFilteredGames,
+  getGames,
+  getSearchedTerm,
+} from '../../store/slices/games';
 import * as Styled from './styled';
 
 const GamesList = () => {
   const dispatch = useAppDispatch();
+  const gamesFiltered = useAppSelector(getFilteredGames);
   const games = useAppSelector(getGames);
+  const searchTerm = useAppSelector(getSearchedTerm);
   const gameStatus = useAppSelector((state) => state.games.status);
+
+  const [gamesList, setGamesList] = useState<IGame[]>([]);
+
+  useEffect(() => {
+    if (searchTerm?.length) {
+      setGamesList(gamesFiltered);
+      return;
+    }
+
+    setGamesList(games);
+  }, [games, searchTerm, gamesFiltered]);
 
   useEffect(() => {
     if (gameStatus === 'idle') {
@@ -16,19 +35,26 @@ const GamesList = () => {
 
   return (
     <Styled.Wrapper>
-      {games.map((game) => (
-        <Styled.ItemWrapper key={game.id}>
-          <Styled.ItemBackground backgroundImg={game?.thumb?.url}>
-            <Styled.ItemBackdrop />
-            <Styled.InternalWrapper>
-              <Styled.ItemTitle>{game.title}</Styled.ItemTitle>
+      {gamesList?.length ? (
+        gamesList.map((game) => (
+          <Styled.ItemWrapper key={game.id}>
+            <Styled.ItemBackground backgroundImg={game?.thumb?.url}>
+              <Styled.ItemBackdrop />
+              <Styled.InternalWrapper>
+                <Styled.ItemTitle>{game.title}</Styled.ItemTitle>
 
-              <Styled.ItemSubTitle>Provider</Styled.ItemSubTitle>
-              <Styled.ItemTitle>{game.providerName}</Styled.ItemTitle>
-            </Styled.InternalWrapper>
-          </Styled.ItemBackground>
-        </Styled.ItemWrapper>
-      ))}
+                <Styled.ItemSubTitle>Provider</Styled.ItemSubTitle>
+                <Styled.ItemTitle>{game.providerName}</Styled.ItemTitle>
+              </Styled.InternalWrapper>
+            </Styled.ItemBackground>
+          </Styled.ItemWrapper>
+        ))
+      ) : (
+        <>
+          <span>No games found</span>
+          {searchTerm && <span>with the seacrhed term "{searchTerm}"</span>}
+        </>
+      )}
     </Styled.Wrapper>
   );
 };
